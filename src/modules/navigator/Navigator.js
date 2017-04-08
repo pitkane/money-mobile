10dc051b697e3aece4cee4fe48bba9f65206efbc
pitkane <mikko@mbit.fi>
2017-04-08 13:02:00 +0300

    Change main screen title

--- src/modules/navigator/Navigator.js
+++ src/modules/navigator/Navigator.js
@@ -1,32 +1,35 @@
-import {Platform} from 'react-native';
-import {TabNavigator, StackNavigator} from 'react-navigation';
+import { Platform } from "react-native";
+import { TabNavigator, StackNavigator } from "react-navigation";
 
-import CounterViewContainer from '../counter/CounterViewContainer';
-import ColorViewContainer from '../colors/ColorViewContainer';
+import CounterViewContainer from "../counter/CounterViewContainer";
+import ColorViewContainer from "../colors/ColorViewContainer";
 
-const headerColor = '#39babd';
-const activeColor = 'white';
+const headerColor = "#39babd";
+const activeColor = "white";
 
 // TabNavigator is nested inside StackNavigator
-export const MainScreenNavigator = TabNavigator({
-  Counter: {screen: CounterViewContainer},
-  Color: {screen: ColorViewContainer}
-}, {
-  tabBarOptions: {
-    ...Platform.select({
-      android: {
-        activeTintColor: activeColor,
-        indicatorStyle: {backgroundColor: activeColor},
-        style: {backgroundColor: headerColor}
-      }
-    })
+export const MainScreenNavigator = TabNavigator(
+  {
+    Counter: { screen: CounterViewContainer },
+    Color: { screen: ColorViewContainer }
+  },
+  {
+    tabBarOptions: {
+      ...Platform.select({
+        android: {
+          activeTintColor: activeColor,
+          indicatorStyle: { backgroundColor: activeColor },
+          style: { backgroundColor: headerColor }
+        }
+      })
+    }
   }
-});
+);
 
 MainScreenNavigator.navigationOptions = {
-  title: 'Pepperoni App Template',
+  title: "Money Main Screen",
   header: {
-    titleStyle: {color: 'white'},
+    titleStyle: { color: "white" },
     style: {
       backgroundColor: headerColor,
       elevation: 0 // disable header elevation when TabNavigator visible
@@ -36,8 +39,8 @@ MainScreenNavigator.navigationOptions = {
 
 // Root navigator is a StackNavigator
 const AppNavigator = StackNavigator({
-  Home: {screen: MainScreenNavigator},
-  InfiniteColorStack: {screen: ColorViewContainer}
+  Home: { screen: MainScreenNavigator },
+  InfiniteColorStack: { screen: ColorViewContainer }
 });
 
 export default AppNavigator;
