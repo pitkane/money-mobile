0d467d0468e2c87244a041c4d8b7d25631ef2a6c
pitkane <mikko@mbit.fi>
2017-04-08 12:55:49 +0300

    Rename to: Money

--- android/app/src/main/java/com/money/MainApplication.java
+++ android/app/src/main/java/com/money/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pepperoniapptemplate;
+package com.money;
 
 import android.app.Application;
 
--- ios/MoneyTests/MoneyTests.m
+++ ios/MoneyTests/MoneyTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 180
 #define TEXT_TO_LOOK_FOR @"Increment counter"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface MoneyTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation MoneyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
